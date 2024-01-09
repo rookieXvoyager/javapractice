37f5b885b6648b62f35e32f633bfc1121c9d7ea0
Sohan Dhanki <sohandhanki7730@gmail.com>
2024-01-09 17:22:10 +0530

    arranging numbers

---
+++ Ascending.java
@@ -0,0 +1,20 @@
+
+public class Ascending {
+    public static void main(String args[]) {
+
+        int a[] = { 5, 8, 3, 2, 1 };
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] < a[j + 1]) {
+                    int temp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ", ");
+        }
+
+    }
+}
