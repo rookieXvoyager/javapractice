03bd891f3c4afff40700a4edb7a9df119d5ebd5e
Sohan Dhanki <sohandhanki7730@gmail.com>
2024-01-09 17:26:07 +0530

    added comment to ascending

--- Ascending.java
+++ Ascending.java
@@ -6,7 +6,7 @@ public class Ascending {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length - 1 - i; j++) {
                 if (a[j] < a[j + 1]) {
-                    int temp = a[j + 1];
+                    int temp = a[j + 1];// main logic, allot any no. to temp and replace it by the other, finally replace temp with the other var
                     a[j + 1] = a[j];
                     a[j] = temp;
                 }
