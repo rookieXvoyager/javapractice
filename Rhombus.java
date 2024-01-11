14e18b9283639298fe8ec5b9f39288f7ed6ef587
Sohan Dhanki <sohandhanki7730@gmail.com>
2024-01-11 08:49:33 +0530

    prints a parallelogram with adjacent sides equal

---
+++ Rhombus.java
@@ -0,0 +1,15 @@
+public class Rhombus {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6 - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 6; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
+    }
+
+}
